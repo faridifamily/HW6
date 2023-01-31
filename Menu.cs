1b2522a5d1fe25cff8748422de22ddca88f50dc4
faridifamily <124078542+faridifamily@users.noreply.github.com>
2023-01-31 21:07:45 +0330

    First Commit

---
+++ Menu.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6
+{
+    public static class Menu
+    {
+        public static void MainMenu() 
+        {
+            Console.WriteLine("Welcome");
+            Console.WriteLine("Please select a number");
+            Console.WriteLine("1. Create a user");
+            Console.WriteLine("2. List of all users");
+            Console.WriteLine("3. Update a user information");
+            Console.WriteLine("4. Delete a user");
+            var input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("Input name");
+                    var name = Console.ReadLine();
+                    Console.WriteLine("Input mobile");
+                    var mobile = long.Parse(Console.ReadLine());
+                    Console.WriteLine("Input birthdate");
+                    var birthDate = DateTime.Parse(Console.ReadLine());
+                    UserRepository u1 = new();
+                    u1.Create(name, mobile, birthDate);
+                    break;
+                case "2":
+                    UserRepository u2 = new();
+                    var list = u2.List();
+                    foreach (var item in list)
+                    { 
+                        Console.WriteLine($"{item.Id},{item.Name},{item.Mobile},{item.BirthDate},{item.CreatedTime}"); 
+                    }
+                    break;
+                case "3":
+                    Console.WriteLine("Input your user id to edit");
+                    int id = int.Parse(Console.ReadLine());
+                    UserRepository u3 = new();
+                    var user = u3.Detail(id);
+                    Console.WriteLine("Do you want to edit name? press enter to skip edit it:");
+                    string editName = Console.ReadLine();
+                    if (editName != "")
+                    {
+                        user.Name = editName;
+                    }
+                    Console.WriteLine("Do you want to edit mobile? press enter to skip edit it:");
+                    string editMobile = Console.ReadLine();
+                    if (editMobile != "")
+                    {
+                        user.Mobile = long.Parse(editMobile);
+                    }
+                    Console.WriteLine("Do you want to edit birthDate? press enter to skip edit it:");
+                    string editBirthDate = Console.ReadLine();
+                    if (editBirthDate != "")
+                    {
+                        user.BirthDate = DateTime.Parse(editBirthDate);
+                    }
+                    u3.Update(id, user);
+                    break;
+                case "4":
+                    Console.WriteLine("Input your user id to delete");
+                    int deleteId = int.Parse(Console.ReadLine());
+                    UserRepository u4 = new();
+                    u4.Delete(deleteId);
+                    break;
+                default:
+                    MainMenu();
+                    break;
+            }
+        }
+    }
+}
---
+++ UserRepository.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6
+{
+    public class UserRepository:IUserRepository
+    {
+        string path = @"D:\HW6\FileDataStorage.csv";
+        public User Create(string name, long mobile, DateTime birthDate)
+        {
+            var users = List();
+            User lastUser = new();
+            if (users.Any())
+            {
+                lastUser = users[users.Count - 1];
+            }
+            User newUser = new();
+            newUser.Id = lastUser.Id + 1;
+            if (lastUser.Id == 0)
+            {
+                newUser.Id = 1;
+            }
+            newUser.Name = name;
+            newUser.Mobile = mobile;
+            newUser.BirthDate = birthDate;
+            newUser.CreatedTime = DateTime.Now;
+            File.AppendAllText(path, $"{newUser.Id},{name},{mobile},{birthDate},{newUser.CreatedTime}\n");
+            return newUser;
+        }
+        public User Create(User user)
+        {
+            File.AppendAllText(path, $"{user.Id},{user.Name},{user.Mobile},{user.BirthDate},{user.CreatedTime}\n");
+            return user;
+        }
+        //public void Read() { }
+        public List<User> List() 
+        {
+            List<User> list = new();
+            var users = File.ReadAllLines(path);
+            foreach (var user in users)
+            {
+                var props = user.Split(",");
+                User u = new();
+                u.Id = int.Parse(props[0]);
+                u.Name = props[1];
+                u.Mobile= long.Parse(props[2]);
+                u.BirthDate = DateTime.Parse(props[3]);
+                u.CreatedTime = DateTime.Parse(props[4]);
+                list.Add(u);
+            }
+            return list;
+        }
+        public User Detail(int id) 
+        {
+            var list = List();
+            var user = list.Find(x => x.Id == id);
+            if (user is not null)
+            {
+                return user;
+            }
+            return null;
+        }
+
+        public User Update(int id, User inputUser) 
+        {
+            var user = Detail(id);
+            if (user is not null)
+            {
+                var users = List();
+                File.WriteAllText(path, string.Empty);
+                foreach (var item in users)
+                {
+                    if (user.Id == item.Id)
+                    {
+                        Create(inputUser);
+                    }
+                    else
+                    {
+                        Create(item);
+                    }
+                }
+            }
+            return user;
+        }
+        public void Delete(int id) 
+        {
+            var users = List();
+            File.WriteAllText(path, string.Empty);
+            foreach (var user in users)
+            {
+                if (user.Id != id)
+                {
+                    Create(user);
+                }
+            }
+        }
+    }
+}
